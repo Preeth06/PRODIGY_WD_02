9b85f3b0d878f1566892bf97df2d6d83e28d58d7
Preeth06 <preethika.alone@gmail.com>
2024-11-30 11:19:59 -0800

    script.js

--- Weather.App/script.js
+++ Weather.App/script.js
@@ -1,45 +1,90 @@
-let result = document.getElementById("result");
-let searchBtn = document.getElementById("search-btn");
-let cityRef = document.getElementById("city");
-let getWeather = () => {
-    let cityValue = cityRef.value;
-    if (cityValue.length == 0) {
+const result = document.getElementById("result");
+const searchBtn = document.getElementById("search-btn");
+const locationBtn = document.getElementById("location-btn");
+const cityRef = document.getElementById("city");
+const key = "1602578432039aed20e19af607f6c2ce";
+const getWeather = (city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+
+    fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+            if (data.cod === "404") {
+                result.innerHTML = `<h3 class="msg">City not found</h3>`;
+                return;
+            }
+
+            result.innerHTML = `
+                        <h2>${data.name}</h2>
+                        <h4 class="weather">${data.weather[0].main}</h4>
+                        <h4 class="desc">${data.weather[0].description}</h4>
+                        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+                        <h1>${data.main.temp} &#176;</h1>
+                        <div class="temp-container">
+                            <div>
+                                <h4 class="title">min</h4>
+                                <h4 class="temp">${data.main.temp_min}&#176;</h4>
+                            </div>
+                            <div>
+                                <h4 class="title">max</h4>
+                                <h4 class="temp">${data.main.temp_max}&#176;</h4>
+                            </div>
+                        </div>
+                    `;
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="msg">Unable to fetch weather data</h3>`;
+        });
+};
+
+const getWeatherByCity = () => {
+    const cityValue = cityRef.value.trim();
+    if (cityValue.length === 0) {
         result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
     } else {
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+        getWeather(cityValue);
         cityRef.value = "";
-        fetch(url)
-            .then((resp) => resp.json())
-            .then((data) => {
-                console.log(data);
-                console.log(data.weather[0].icon);
-                console.log(data.weather[0].main);
-                console.log(data.weather[0].description);
-                console.log(data.name);
-                console.log(data.main.temp_min);
-                console.log(data.main.temp_max);
-                result.innerHTML = `
-        <h2>${data.name}</h2>
-        <h4 class="weather">${data.weather[0].main}</h4>
-        <h4 class="desc">${data.weather[0].description}</h4>
-        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <h1>${data.main.temp} &#176;</h1>
-        <div class="temp-container">
-            <div>
-                <h4 class="title">min</h4>
-                <h4 class="temp">${data.main.temp_min}&#176;</h4>
-            </div>
-            <div>
-                <h4 class="title">max</h4>
-                <h4 class="temp">${data.main.temp_max}&#176;</h4>
-            </div>
-        </div>
-        `;
-            })
-            .catch(() => {
-                result.innerHTML = `<h3 class="msg">City not found</h3>`;
-            });
     }
 };
-searchBtn.addEventListener("click", getWeather);
-window.addEventListener("load", getWeather);
+
+const getWeatherByLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
+
+            fetch(url)
+                .then((resp) => resp.json())
+                .then((data) => {
+                    result.innerHTML = `
+                                <h2>${data.name}</h2>
+                                <h4 class="weather">${data.weather[0].main}</h4>
+                                <h4 class="desc">${data.weather[0].description}</h4>
+                                <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+                                <h1>${data.main.temp} &#176;</h1>
+                                <div class="temp-container">
+                                    <div>
+                                        <h4 class="title">min</h4>
+                                        <h4 class="temp">${data.main.temp_min}&#176;</h4>
+                                    </div>
+                                    <div>
+                                        <h4 class="title">max</h4>
+                                        <h4 class="temp">${data.main.temp_max}&#176;</h4>
+                                    </div>
+                                </div>
+                            `;
+                })
+                .catch(() => {
+                    result.innerHTML = `<h3 class="msg">Unable to fetch weather data</h3>`;
+                });
+        }, () => {
+            result.innerHTML = `<h3 class="msg">Geolocation not supported or permission denied</h3>`;
+        });
+    } else {
+        result.innerHTML = `<h3 class="msg">Geolocation not supported by your browser</h3>`;
+    }
+};
+
+searchBtn.addEventListener("click", getWeatherByCity);
+locationBtn.addEventListener("click", getWeatherByLocation);
+window.addEventListener("load", getWeatherByCity);
